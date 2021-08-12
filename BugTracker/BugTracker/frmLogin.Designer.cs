c50d4591faa360cecca0713744cc50ff7e7d0d81
Ivan Gomez <ivangomez8546@gmail.com>
2021-08-11 21:31:53 -0300

    actividad terminada

--- BugTracker/BugTracker/frmLogin.Designer.cs
+++ BugTracker/BugTracker/frmLogin.Designer.cs
@@ -78,6 +78,7 @@ namespace BugTracker
             this.btnIngresar.TabIndex = 4;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
             // 
             // btnSalir
             // 
@@ -87,6 +88,7 @@ namespace BugTracker
             this.btnSalir.TabIndex = 5;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // frmLogin
             // 
@@ -100,7 +102,7 @@ namespace BugTracker
             this.Controls.Add(this.lblnContraseña);
             this.Controls.Add(this.lblnUsuario);
             this.Name = "frmLogin";
-            this.Text = "frmLogin";
+            this.Text = "Inicio de Sesion";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- BugTracker/BugTracker/frmLogin.cs
+++ BugTracker/BugTracker/frmLogin.cs
@@ -14,5 +14,27 @@ namespace BugTracker
         {
             InitializeComponent();
         }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (this.txtUsuario.Text == "")
+            {
+                MessageBox.Show("Se debe ingresar una contraseña.");
+                return;
+            }
+            if(this.txtContraseña.Text == "")
+            {
+                MessageBox.Show("Se debe ingresar una contraseña.");
+                return;
+            }
+
+            MessageBox.Show("Usted a ingresado al sistema.");
+            this.Close();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
