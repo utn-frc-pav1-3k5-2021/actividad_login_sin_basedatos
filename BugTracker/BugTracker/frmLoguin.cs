42621610ba15e8c5a10013b5ff957a7d75cb2fdf
Santiago Garay <Santidmlv@gmail.com>
2021-08-11 21:36:40 -0300

    primer cambio

---
+++ BugTracker/BugTracker/frmLoguin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BugTracker
+{
+    public partial class frmLoguin : Form
+    {
+        public frmLoguin()
+        {
+            InitializeComponent();
+        }
+    }
+}
