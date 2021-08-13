073c1c347a87e69ed75b6f3f28ebe149702d57c7
Agustin Bessone <bessone98@gmail.com>
2021-08-13 16:59:32 -0300

    cambios iniciales

---
+++ BugTracker/BugTracker/frmLogin.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace BugTracker
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_usuario = new System.Windows.Forms.Label();
+            this.lbl_contraseña = new System.Windows.Forms.Label();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.txt_contraseña = new System.Windows.Forms.TextBox();
+            this.btn_ingresar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_usuario
+            // 
+            this.lbl_usuario.AutoSize = true;
+            this.lbl_usuario.Location = new System.Drawing.Point(71, 41);
+            this.lbl_usuario.Name = "lbl_usuario";
+            this.lbl_usuario.Size = new System.Drawing.Size(59, 20);
+            this.lbl_usuario.TabIndex = 0;
+            this.lbl_usuario.Text = "Usuario";
+            // 
+            // lbl_contraseña
+            // 
+            this.lbl_contraseña.AutoSize = true;
+            this.lbl_contraseña.Location = new System.Drawing.Point(71, 80);
+            this.lbl_contraseña.Name = "lbl_contraseña";
+            this.lbl_contraseña.Size = new System.Drawing.Size(83, 20);
+            this.lbl_contraseña.TabIndex = 1;
+            this.lbl_contraseña.Text = "Contraseña";
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(186, 34);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(125, 27);
+            this.txt_usuario.TabIndex = 2;
+            // 
+            // txt_contraseña
+            // 
+            this.txt_contraseña.Location = new System.Drawing.Point(186, 73);
+            this.txt_contraseña.Name = "txt_contraseña";
+            this.txt_contraseña.Size = new System.Drawing.Size(125, 27);
+            this.txt_contraseña.TabIndex = 3;
+            // 
+            // btn_ingresar
+            // 
+            this.btn_ingresar.Location = new System.Drawing.Point(102, 135);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Size = new System.Drawing.Size(94, 29);
+            this.btn_ingresar.TabIndex = 4;
+            this.btn_ingresar.Text = "Ingresar";
+            this.btn_ingresar.UseVisualStyleBackColor = true;
+            this.btn_ingresar.Click += new System.EventHandler(this.btn_ingresar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(217, 135);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(94, 29);
+            this.btn_salir.TabIndex = 5;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 191);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_ingresar);
+            this.Controls.Add(this.txt_contraseña);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.lbl_contraseña);
+            this.Controls.Add(this.lbl_usuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_usuario;
+        private System.Windows.Forms.Label lbl_contraseña;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox txt_contraseña;
+        private System.Windows.Forms.Button btn_ingresar;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
+
---
+++ BugTracker/BugTracker/frmLogin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugTracker
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ingresar_Click(object sender, EventArgs e)
+        {
+            if (txt_usuario.Text == "") {
+                MessageBox.Show("Ingrese un usuario");
+                return;
+            }
+
+            if (txt_contraseña.Text == "")
+            {
+                MessageBox.Show("Ingrese una contraseña");
+                return;
+            }
+
+            MessageBox.Show("Ha ingresado exitosamente al sistema!!");
+            this.Close();
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
