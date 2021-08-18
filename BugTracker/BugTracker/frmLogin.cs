47080a7e7ecfbae9ce9180dcab558e2f23f54d7b
noel araoz <noelaraoz123@gmail.com>
2021-08-17 21:49:36 -0300

    Paso a Paso hecho

---
+++ BugTracker/BugTracker/frmLogin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BugTracker
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
