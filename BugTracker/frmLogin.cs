061112b2809a8f33988a92005a5bc9063d95eeec
82830-Federica-Zabala <feduzabala@hotmail.com>
2021-08-17 22:54:37 -0300

    Tarea Lista

---
+++ BugTracker/frmLogin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BugTracker
+{
+    public partial class frmIS : Form
+    {
+        public frmIS()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            //Validamos que se haya ingresado un usuario
+            if((txtUsuario.Text == ""))
+            {
+                MessageBox.Show("Se debe Ingresar un ususario.");
+                return;
+            }
+
+            //Validamos que se haya ingresado una contraseña
+            if((txtContraseña.Text == ""))
+            {
+                MessageBox.Show("Se debe ingresar una contraseña.");
+                return;
+            }
+
+            MessageBox.Show("Usted a ingresalo al sistema.");
+            this.Close();
+        }
+    }
+}
