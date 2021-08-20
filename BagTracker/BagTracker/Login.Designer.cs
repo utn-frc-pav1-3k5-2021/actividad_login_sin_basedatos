f2b2fe15b4224e8e1b96516e3172b96a64fbbc76
79006 <agostinellirodrigo351@gmail.com>
2021-08-20 15:34:33 -0300

    actualizado

---
+++ BagTracker/BagTracker/Login.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace BagTracker
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblContra = new System.Windows.Forms.Label();
+            this.txtUsu = new System.Windows.Forms.TextBox();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblUsu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblContra
+            // 
+            this.lblContra.AutoSize = true;
+            this.lblContra.Location = new System.Drawing.Point(12, 90);
+            this.lblContra.Name = "lblContra";
+            this.lblContra.Size = new System.Drawing.Size(70, 15);
+            this.lblContra.TabIndex = 1;
+            this.lblContra.Text = "Contraseña:";
+            // 
+            // txtUsu
+            // 
+            this.txtUsu.Location = new System.Drawing.Point(68, 32);
+            this.txtUsu.Name = "txtUsu";
+            this.txtUsu.Size = new System.Drawing.Size(189, 23);
+            this.txtUsu.TabIndex = 2;
+            // 
+            // txtContra
+            // 
+            this.txtContra.Location = new System.Drawing.Point(88, 82);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.Size = new System.Drawing.Size(169, 23);
+            this.txtContra.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnIngresar);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(149, 154);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // lblUsu
+            // 
+            this.lblUsu.AutoSize = true;
+            this.lblUsu.Location = new System.Drawing.Point(12, 40);
+            this.lblUsu.Name = "lblUsu";
+            this.lblUsu.Size = new System.Drawing.Size(50, 15);
+            this.lblUsu.TabIndex = 6;
+            this.lblUsu.Text = "Usuario:";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 189);
+            this.Controls.Add(this.lblUsu);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtContra);
+            this.Controls.Add(this.txtUsu);
+            this.Controls.Add(this.lblContra);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblContra;
+        private System.Windows.Forms.TextBox txtUsu;
+        private System.Windows.Forms.TextBox txtContra;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lblUsu;
+    }
+}
---
+++ BagTracker/BagTracker/Login.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BagTracker
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIngresar(object sender, EventArgs e)
+        {
+            // Validacion de Usuario
+            if((txtUsu.Text == ""))
+            {
+                MessageBox.Show("Por favor ingresar un Usuario...");
+                return;
+            }
+
+
+            // Validacion de Contraseña
+            if((txtContra.Text == ""))
+            {
+                MessageBox.Show("Por favor ingrese su contraseña...");
+                return;
+            }
+
+            MessageBox.Show("Se ha ingresado al sistema...");
+            this.Close();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            // NADA
+        }
+    }
+}
