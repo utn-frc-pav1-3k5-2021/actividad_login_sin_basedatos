d394fbda6aed44c43404da48c5141413478cd017
Victor Bazan <bazan.victor@hotmail.com>
2021-08-25 19:04:41 -0300

    se agrega codigo

--- BugTracker/FrmLogin.cs
+++ BugTracker/FrmLogin.cs
@@ -17,5 +17,50 @@ namespace BugTracker
             InitializeComponent();
         }
 
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "")
+            {
+                MessageBox.Show("Se debe ingresar un usuario", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            if (txtContra.Text=="")
+            {
+                MessageBox.Show("Se debe ingresar una contraseña", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            Usuario usuario = new Usuario();
+            usuario.NombreUsuario = txtUsuario.Text;
+            usuario.Password = txtContra.Text;
+
+            if (ValidarUsuario(usuario))
+            {
+                MessageBox.Show("Ingreso Correcto", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            else
+            {
+                txtContra.Text = "";
+                txtUsuario.Text = "";
+                MessageBox.Show("Usuario o Contraseña Incorrectos", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+
+
+        }
+
+        private bool ValidarUsuario(Usuario usuario)
+        {
+            if (usuario.NombreUsuario == "victor" && usuario.Password == "bazan") return true;
+
+            else return false;
+        }
     }
 }
