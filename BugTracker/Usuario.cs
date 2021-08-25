07e58846440b15260e08e11fc12528d28ea26a54
Victor Bazan <bazan.victor@hotmail.com>
2021-08-25 19:05:03 -0300

    se agrega codigo

---
+++ BugTracker/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTracker
+{
+    class Usuario
+    {
+        public string NombreUsuario { get; set; }
+        public string Password { get; set; }
+
+    }
+}
